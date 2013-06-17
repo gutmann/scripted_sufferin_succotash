8a72398d6f5d87938ed94b342d1e45db6dbd7176
Ethan Gutmann <gutmann@ucar.edu>
2013-06-17 19:32:51 +0000

    more wsc work, added compare,wrf skeleton

---
+++ python/wsc/wrf.py
@@ -0,0 +1,10 @@
+import datetime
+import numpy as np
+from stat_down import myio
+
+def load(filename, startyear=2000,startdate=None):
+    """Load WRF SWE data from a file
+    
+    
+    """
+    pass
