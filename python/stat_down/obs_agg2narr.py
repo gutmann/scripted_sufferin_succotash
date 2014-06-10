88853b85ed7fbc5eb9108046166b04d89e9e6e7e
Ethan Gutmann <gutmann@ucar.edu>
2014-06-10 17:20:28 -0600

    lots of stuff, esp. in wsc

--- python/stat_down/obs_agg2narr.py
+++ python/stat_down/obs_agg2narr.py
@@ -4,28 +4,33 @@ import os
 import copy
 import time
 import numpy as np
-import nio
+import mygis
 import regrid_hi2low
 from bunch import Bunch
 
 # process_var="tasmin"
-process_var="tasmax"
+# process_var="tasmax"
+process_var="tas"
 # process_var="pr"
 
-narr_samplefile="/Volumes/G-SAFE/usbr/statistical/narr_sample.nc"
-obs_filesearch="*"+process_var+".*.nc"
-meta=Bunch(title="Observed regridded to NARR projection",
-    creator=os.environ['USER']+" using obs_agg2narr.py",
-    source="UW via USBR", conventions="None")
-
-# obs_filesearch="nldas_met*.nc"
+# narr_samplefile="/Volumes/G-SAFE/usbr/statistical/narr_sample.nc"
+if process_var=="pr":
+    narr_samplefile="/d5/gutmann/cc-downscaling-test/ccsm/pr/prate.run5.complete.1960-2080.nc"
+elif process_var=="tas":
+    narr_samplefile="/d5/gutmann/cc-downscaling-test/ccsm/tas/tas.run5.complete.1960-2080.nc"
+# obs_filesearch="*"+process_var+".*.nc"
 # meta=Bunch(title="Observed regridded to NARR projection",
 #     creator=os.environ['USER']+" using obs_agg2narr.py",
-#     source="Maurer via USBR", conventions="None")
+#     source="UW via USBR", conventions="None")
+
+obs_filesearch="nldas_met*.nc"
+meta=Bunch(title="Observed regridded to NARR projection",
+    creator=os.environ['USER']+" using obs_agg2narr.py",
+    source="Maurer via USBR", conventions="None")
 
 
 def read_base(filename):
-    f=nio.open_file(filename,mode="r")
+    f=mygis.Dataset(filename,mode="r")
     output=Bunch()
 
     variables=Bunch()
@@ -36,27 +41,21 @@ def read_base(filename):
         else:
             data=Bunch()
         data.atts=copy.deepcopy(curvar.__dict__)
-        data.dims=copy.deepcopy(curvar.dimensions)
-        data.dtype=curvar.typecode()
+        # data.dims=copy.deepcopy(curvar.dimensions)
+        data.dims=curvar.dimensions
+        data.dtype=curvar.dtype
         variables[k]=data
         
     output.variables=variables
     output.global_attributes=copy.deepcopy(f.__dict__)
-    output.dimensions=copy.deepcopy(f.dimensions)
-    f.close()
+    # output.dimensions=copy.deepcopy(f.dimensions)
+    output.dimensions=f.dimensions
+    output.ncfile=f
+    # f.close()
     return output
-    
-def read_geo(filename):
-    f=nio.open_file(filename,mode="r")
-    lat=f.variables["lat"][:]
-    lon=f.variables["lon"][:]
-    if lon.max()>180:
-        lon=lon-360
-    f.close()
-    return Bunch(lat=lat,lon=lon)
 
-def write_data(filename,data,nbase,obase,outputvar="pr"):
-    of=nio.open_file(filename,mode='w')
+def write_data(filename,data,nbase,obase,outputvar="pr",fillvalue=None):
+    of=mygis.Dataset(filename,mode='w')
     of.title=meta.title
     of.creator=meta.creator
     of.creation_date=time.ctime()
@@ -70,36 +69,64 @@ def write_data(filename,data,nbase,obase,outputvar="pr"):
     
     for dimkey in nbase.dimensions.keys():
         if dimkey=="time":
-            of.create_dimension("time",0)
+            of.createDimension("time",0)
         else:
-            of.create_dimension(dimkey,nbase.dimensions[dimkey])
-    
+            of.createDimension(dimkey,int(str(nbase.dimensions[dimkey]).split("=")[-1]))
+            # of.createDimension(dimkey,int(nbase.dimensions[dimkey]))
     for var in nbase.variables.keys():
+        
+        # Create the variable in the output netcdf file
         curvar=nbase.variables[var]
-        if (var!="pr"):
-            outvar=of.create_variable(var,curvar.dtype,tuple(curvar.dims))
+        if (var!="pr") and (var!="tas") and (var!="tasmax") and (var!="tasmin"):
+            if var=="time":
+                try:
+                    curvar=obase.variables[var]
+                    outvar=of.createVariable(var,curvar.dtype,tuple(curvar.dims))
+                except:
+                    print("ERROR managing time")
+            elif var=="time_bnds":
+                pass
+            else:
+                outvar=of.createVariable(var,curvar.dtype,tuple(curvar.dims))
         else:
-            outvar=of.create_variable(outputvar,curvar.dtype,tuple(curvar.dims))
-        for k in curvar.atts.keys():
-            setattr(outvar,k,curvar.atts[k])
-        if (var=="pr"): # or (var=="tasmin") or (var=="tasmax"):
-            setattr(outvar,"units",getattr(obase.variables,outputvar).atts["units"])
+            curvar=obase.variables[var]
+            outputdims=[]
+            for thisdim in curvar.dims:
+                if thisdim=="latitude":
+                    thisdim="lat"
+                if thisdim=="longitude":
+                    thisdim="lon"
+                outputdims.append(thisdim)
+                    
+            outvar=of.createVariable(outputvar,curvar.dtype,tuple(outputdims),fill_value=fillvalue)
+            
+        # copy attributes in
+        if (var!="time_bnds"):
+            for k in curvar.atts.keys():
+                if k!="_FillValue":
+                    setattr(outvar,k,curvar.atts[k])
+        
+        # finally copy the data in
+        if (var=="pr") or (var=="tasmin") or (var=="tasmax") or (var=="tas"):
+            try:
+                setattr(outvar,"units",getattr(obase.variables,outputvar).atts["units"])
+            except:
+                pass
             for i in range(data.shape[0]):
                 outvar[i,:,:]=data[i,:,:].astype(curvar.dtype)
         elif var=="time":
             obs_time=obase.variables.time.data
             for i in range(obs_time.size):
                 outvar[i]=obs_time[i].astype(curvar.dtype)
+        elif var=="time_bnds":
+            pass
         else:
             outvar[:]=curvar.data[:].astype(curvar.dtype)
     of.close()
     
 def read_obs(filename,var="pr"):
-    f=nio.open_file(filename,mode="r")
-    data=f.variables[var][:]
-    f.close()
-    return data
-            
+    return mygis.read_nc(filename,var).data
+                
 def main(root_dir=None,var=None,filesearch=None):
     if var==None:
         var=process_var
@@ -110,14 +137,21 @@ def main(root_dir=None,var=None,filesearch=None):
         obs_filesearch=root_dir+obs_filesearch
     else:
         root_dir=""
-    narrgeo=read_geo(narr_samplefile)
+    narrgeo=mygis.read_geo(narr_samplefile)
     narrbase=read_base(narr_samplefile)
     files=glob.glob(obs_filesearch)
-    obsgeo=read_geo(files[0])
-    obslon,obslat=np.meshgrid(obsgeo.lon,obsgeo.lat)
+    obsgeo=mygis.read_geo(files[0])
+    if len(obsgeo.lon.shape)==1:
+        obslon,obslat=np.meshgrid(obsgeo.lon,obsgeo.lat)
+    else:
+        obslon,obslat=(obsgeo.lon,obsgeo.lat)
     print("computing Geo LUT")
     geoLUT=regrid_hi2low.load_geoLUT(obslat,obslon,narrgeo.lat,narrgeo.lon)
-    fillvalue=narrbase.variables.pr.atts["_FillValue"][0]
+        
+    try:
+        fillvalue=narrbase.variables.pr.atts["_FillValue"][0]
+    except:
+        fillvalue=1e20
     for f in files:
         print("Working on : "+f)
         obs_data=read_obs(f,var=var)
@@ -125,7 +159,7 @@ def main(root_dir=None,var=None,filesearch=None):
         outputdata=regrid_hi2low.regrid_hi2low(obs_data,geoLUT=geoLUT,FillValue=fillvalue)
         outputfile="regridded_"+f.split('/')[-1]
         print("  writing...")
-        write_data(root_dir+outputfile,outputdata.data,narrbase,obs_base,outputvar=var)
+        write_data(root_dir+outputfile,outputdata.data,narrbase,obs_base,outputvar=var,fillvalue=fillvalue)
 
 if __name__ == '__main__':
     main()
