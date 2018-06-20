53e7fee0ab386a841fa13b6fb14d87dd2e4bfa89
Ethan Gutmann <gutmann@ucar.edu>
2018-06-20 10:25:33 -0600

    wsc update

--- python/wsc/__init__.py
+++ python/wsc/__init__.py
@@ -1,3 +1,3 @@
-import regrid, snodas, recon, wrf, compare, regrid, modscag, nlcd, compare2lidar
-
-__all__ = ['recon', 'snodas','wrf','regrid','compare','modscag',"nlcd",'compare2lidar']
+# import regrid, snodas, recon, wrf, compare, regrid, modscag, nlcd, compare2lidar
+#
+# __all__ = ['recon', 'snodas','wrf','regrid','compare','modscag',"nlcd",'compare2lidar']
