18b218798462d24e5ab5037cddc8ccb127482057
Ethan Gutmann <gutmann@ucar.edu>
2018-06-20 10:27:30 -0600

    update interception video scripts

--- python/interception/vid_fft.py
+++ python/interception/vid_fft.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from __future__ import print_function
+# from __future__ import print_function
 import sys, os
 import glob
 
@@ -10,7 +10,7 @@ import video_reader as vr
 import mygis
 
 global verbose
-verbose=False
+verbose=True
 
 global parallel
 parallel=False
@@ -25,9 +25,9 @@ fps = 30.0
 def read_vid(filename="/Users/gutmann/Desktop/IMG_2303.m4v",resolution=(1080,1920,3),n=-1):
     """docstring for read_vid"""
     vid=vr.Video_Reader(filename,resolution)
-    
+
     if verbose:print("Reading:"+filename)
-    
+
     outputdata=[]
     if n>0:
         for i in range(n):
@@ -39,23 +39,23 @@ def read_vid(filename="/Users/gutmann/Desktop/IMG_2303.m4v",resolution=(1080,192
             sys.stdout.flush()
             outputdata.append(v[np.newaxis,:,:,:])
             i+=1
-    
+
     if verbose:print("\nFinished reading:"+filename)
-    
+
     outputdata=np.concatenate(outputdata,axis=0)
     return outputdata
 
 def compute_fft(i,data_subset):
-    
+
     for_fft_data = data_subset[:,:,i:]
-    
+
     freq_space = np.fft.fft(for_fft_data,axis=2)
 
     frequencies = np.fft.fftfreq(for_fft_data.shape[2], d=1/fps) [ 10:60 ]
 
     maxf = frequencies[np.argmax(np.abs(freq_space[:,:,10:60].real) + np.abs(freq_space[:,:,-10:-60:-1].real),axis=2)]
     f_ampl = np.max(np.abs(freq_space[:,:,10:60].real) + np.abs(freq_space[:,:,-10:-60:-1].real),axis=2)
-    
+
     return maxf, f_ampl
 
 if parallel:
@@ -74,24 +74,24 @@ def compute_frequency(data):
     # xmin=800
     # xmax=1050
     xstep = 1 if (xmax > xmin) else -1
-    
+
     ymin=750
     ymax=450
     # ymin=1080
     # ymax=1080-400
     # ymax=1080-200
     ystep = 1 if (ymax > ymin) else -1
-    
+
     data_subset = np.transpose(data[:,ymin:ymax:ystep,xmin:xmax:xstep,0],axes=[1,2,0])
 
     maxf = np.zeros((number_of_fft, data_subset.shape[0], data_subset.shape[1]))
     f_ampl = np.zeros((number_of_fft, data_subset.shape[0], data_subset.shape[1]))
-    
+
     if parallel:
         res=[]
-        
+
     for i in range(number_of_fft):
-        
+
         if parallel:
             if verbose: print("\r Starting FFT:{:3}".format(i+1), end="")
             res.append(p.apply_async(compute_fft, (i,data_subset)))
@@ -108,33 +108,42 @@ def compute_frequency(data):
             if verbose: print("\r Completed FFT:{:3}".format(i+1), end="")
             sys.stdout.flush()
             maxf[i], f_ampl[i] = res[i].get()
-    
+
     if verbose:print("\r Completed all FFT computations")
-    
-    
+
+
     # tmp=np.argmax(f_ampl,axis=0).astype("uint8")
     # bestf=np.zeros(tmp.shape)
     # for i in range(tmp.shape[0]):
     #         for j in range(tmp.shape[1]):
     #                 bestf[i,j]=maxf[tmp[i,j],i,j]
-    # 
+    #
     # besta = f_ampl.max(axis=0)
     # bestf = np.ma.array(bestf, mask = besta<1500)
     # return (bestf, besta)
-    
+
     return (maxf, f_ampl)
-    
+
 def write(filename, data):
     mygis.write(filename,data)
-    
+
 def make_image(data, inputfile):
     plt.clf()
     plt.imshow(data[0].astype('uint8'),origin="upper")
     plt.savefig(inputfile.split(".")[0]+".png")
 
+
+    plt.clf()
+    print(data.shape)
+    plt.imshow(data[:,:,:,1].std(axis=0),origin="upper")
+    plt.colorbar()
+    plt.clim(5,25)
+    plt.savefig(inputfile.split(".")[0]+"_std.png")
+
+
 def complete(filename, outputdir=""):
     return os.path.isfile(outputdir+"ampl_"+filename.split("/")[-1][:-4]+".nc")
-        
+
 
 def main(filesearch, outputdir="output3/"):
     files=glob.glob(filesearch)
@@ -146,12 +155,12 @@ def main(filesearch, outputdir="output3/"):
             data = read_vid(filename=f)
             make_image(data, f)
             freq, ampl = compute_frequency(data)
-            
+
             write(outputdir+"freq_"+f.split("/")[-1][:-4]+".nc", freq)
             write(outputdir+"ampl_"+f.split("/")[-1][:-4]+".nc", ampl)
         else:
             print("Already completed:"+f.split("/")[-1])
-    
+
 
 if __name__ == '__main__':
     verbose=True
--- python/interception/video_reader.py
+++ python/interception/video_reader.py
@@ -11,7 +11,7 @@ class Video_Reader(object):
     command=None
     pipe=None
     resolution=None
-    
+
     def __init__(self, filename,resolution):
         super(Video_Reader, self).__init__()
         self.command = [ FFMPEG,
@@ -21,46 +21,48 @@ class Video_Reader(object):
                     '-vcodec', 'rawvideo', '-']
         # note: stdout pipes the data we want
         # stderr captures the status updates ffmpeg would print to the screen
-        # stdin prevents ffmpeg from capturing keyboard input. 
-        self.pipe = sp.Popen(self.command, bufsize=BUFFER_SIZE, 
+        # stdin prevents ffmpeg from capturing keyboard input.
+        self.pipe = sp.Popen(self.command, bufsize=BUFFER_SIZE,
                              stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE)
-        
-        
+
+
         self.resolution=resolution
         self.npixels=resolution[0]*resolution[1]*resolution[2]
-        
+
     def __iter__(self):
         """make the reader object iterable"""
         return self
-        
+
     def __next__(self):
         try:
             raw_image = self.pipe.stdout.read(self.npixels)
         except:
             # if something breaks here, the pipe may already be closed
             raise StopIteration
-        
+
         if (len(raw_image)<self.resolution[0]*self.resolution[1]*self.resolution[2]):
             self.close()
             raise StopIteration
-        
+
         image = np.fromstring(raw_image, dtype='uint8')
-        
+
         return image.reshape(self.resolution)
-    
+
     next=__next__
-    
+
     def close(self):
         """docstring for close"""
         self.pipe.stdout.close()
         self.pipe.stderr.close()
         self.pipe.send_signal(signal.SIGINT)
         self.pipe.wait()
-        
+
 
 def main():
-    """docstring for main"""
-    print("This is a library for reading h.264 video strings into python via ffmpeg")
+    """Print purpose of library"""
+    print("This is a library for reading video sequences into python via ffmpeg. ")
+    print("Provides the 'Video_Reader' iterator class. ")
+    print("Requires ffmpeg be installed. ")
 
 if __name__ == '__main__':
     main()
